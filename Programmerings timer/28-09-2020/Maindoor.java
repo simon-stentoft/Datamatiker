3ad31df9580206a1316cc3e501c8bdeafc95ac7c
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-09-28 11:42:19 +0200

    Create Maindoor.java

---
+++ Programmerings timer/28-09-2020/Maindoor.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        // Doorlock der låser døren af sig selv og åbner når tilladte personer kommer tæt på udefra, Afføringsmåler, vækkeur der justerer sig selv baseret på hvornår jeg går i seng.
+
+        
+    }
+}
