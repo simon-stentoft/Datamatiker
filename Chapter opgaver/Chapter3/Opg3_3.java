d4dfe7fb3694d67ebfb6d4c991666eab98891852
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-09-29 12:57:23 +0200

    Create Opg3_3.java

---
+++ Chapter opgaver/Chapter3/Opg3_3.java
@@ -0,0 +1,26 @@
+package Chapter3;
+import java.util.Scanner;
+public class Opg3_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a value for a, b, c, d, e and f: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double d = input.nextDouble();
+        double e = input.nextDouble();
+        double f = input.nextDouble();
+
+        if (a*d - b*c == 0)
+            System.out.println("The equation has no solution");
+        else
+        {
+            double x = (e * d - b * f) / (a * d - b * c);
+            double y = (a * f - e * c) / (a * d - b * c);
+            System.out.println("x is " + x + " and y is " + y);
+        }
+
+
+    }
+}
