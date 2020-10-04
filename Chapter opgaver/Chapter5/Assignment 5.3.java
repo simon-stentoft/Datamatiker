a4773e7d18d7a5e1abb4290747292591645303ae
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-10-04 15:21:35 +0200

    Create Assignment 5.3.java

---
+++ Chapter opgaver/Chapter5/Assignment 5.3.java
@@ -0,0 +1,14 @@
+public class Assignment5_3 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Celsius        Farenheit");
+        for (double celsius = 0; celsius <= 100; celsius++) {
+            final double fahrenheit = (celsius * 9/5) + 32;
+            if (celsius % 2 == 0) {
+                System.out.println(celsius+"            "+ fahrenheit);
+
+            }
+        }
+    }
+}
