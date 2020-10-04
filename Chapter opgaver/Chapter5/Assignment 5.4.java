7ec889a9ff5eba6ee7c0337913b55b6601eb9f58
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-10-04 15:21:53 +0200

    Create Assignment 5.4.java

---
+++ Chapter opgaver/Chapter5/Assignment 5.4.java
@@ -0,0 +1,12 @@
+public class Ass5_4 {
+    public static void main(String[] args) {
+
+        System.out.println("Inches\tCentimetres");
+
+        for (int i = 1; i <= 10; i++) {
+            double centimetres = i * 2.54;
+            System.out.println(i+"\t\t"+ centimetres);
+
+        }
+    }
+}
