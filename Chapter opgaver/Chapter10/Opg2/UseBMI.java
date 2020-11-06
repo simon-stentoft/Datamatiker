c1696929287d4677df410f2a2c53495b1a897d96
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-11-06 13:50:03 +0100

    e

---
+++ Chapter opgaver/Chapter10/Opg2/UseBMI.java
@@ -0,0 +1,16 @@
+package Chapter10.Opg2;
+
+import Chapter10.Opg2.BMI;
+
+public class UseBMI {
+    public static void main(String[] args) {
+        BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
+        System.out.println("The BMI for " + bmi1.getName() + " is " + bmi1.getBMI() + " " + bmi1.getStatus());
+
+        BMI bmi2 = new BMI("Susan", 215, 70);
+        System.out.println("The BMI for " + bmi2.getName() + " is " + bmi2.getBMI() + " " + bmi2.getStatus());
+
+        BMI bmi3 = new BMI("Simon", 24, 152, 5,8);
+        System.out.println("The BMI for " + bmi3.getName() + " is " + bmi3.getBMI() + " " + bmi3.getStatus());
+    }
+}
